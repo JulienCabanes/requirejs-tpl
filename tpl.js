eded39267a576c4281d0bdfd38ed292dcd4e4a0d
Dawson Toth <dawson@tothsolutions.com>
2013-01-17 15:23:45 -0600

    Fixed Bundling
    Use text.js for bundling too. Keeps the code very simple.

--- tpl.js
+++ tpl.js
@@ -3,32 +3,79 @@
  *
  * Uses UnderscoreJS micro-templates : http://documentcloud.github.com/underscore/#template
  * @author Julien Cabanès <julien@zeeagency.com>
- * @version 0.2
- * 
+ * @version 0.3
+ *
  * @license RequireJS text 0.24.0 Copyright (c) 2010-2011, The Dojo Foundation All Rights Reserved.
  * Available via the MIT or new BSD license.
  * see: http://github.com/jrburke/requirejs for details
  */
-/*jslint regexp: false, nomen: false, plusplus: false, strict: false */
-/*global require: false, XMLHttpRequest: false, ActiveXObject: false,
-  define: false, window: false, process: false, Packages: false,
-  java: false */
 
-define(['text', 'underscore'], function(text, _)
+define(['module', 'text', 'underscore'], function(module, text, _)
 {
-	var baseFinishLoad = text.finishLoad;
-	text.finishLoad = function()
-	{
-		var baseOnLoad = arguments[arguments.length - 1];
-		arguments[arguments.length - 1] = function(content)
+	var settings = { variable: 'data' };
+
+	return {
+		/**
+		 * Dynamically loads your module by using text! to require the content, and then return the results of _.template.
+		 * @param name
+		 * @param req
+		 * @param onLoad
+		 * @param config
+		 */
+		load: function(name, req, onLoad, config)
 		{
-			if (content)
+			if (config.isBuild)
 			{
-				content = _.template(content);
+				text.load.apply(text, arguments);
 			}
-			baseOnLoad(content);
-		};
-		baseFinishLoad.apply(this, arguments);
+
+			req([ 'text!' + name ], function(content)
+			{
+				onLoad(content
+					? _.template(content, undefined, settings)
+					: function() { return ''; });
+			});
+		},
+		/**
+		 * Used by the optimizer to compile your templates. Logs detailed error messages whenever an invalid template is
+		 * encountered.
+		 * @param pluginName
+		 * @param moduleName
+		 * @param write
+		 * @param config
+		 */
+		write: function(pluginName, moduleName, write, config)
+		{
+			text.write.call(text, pluginName, moduleName, {
+				asModule: function(name, contents)
+				{
+					contents = contents.substr(contents.indexOf('return') + 7);
+					contents = contents.substr(0, contents.lastIndexOf(';') - 3);
+					try
+					{
+						var template = _.template(escape(contents), undefined, settings);
+						write.asModule(pluginName + "!" + moduleName,
+							"define(function() { return " + template.source + " });");
+					}
+					catch (err)
+					{
+						console.error('~~~~~');
+						console.error('FAILED TO COMPILE ' + pluginName + "!" + moduleName);
+						console.error('Error:\t\t' + String(err));
+						console.error('\n');
+						if (err && err.source)
+						{
+							console.error('Error Source:\n');
+							console.error(err.source);
+							console.error('\n\n');
+						}
+						console.error('Original Contents:\n');
+						console.error(contents);
+						console.error('\n\n');
+						throw err;
+					}
+				}
+			}, config);
+		}
 	};
-	return text;
 });
